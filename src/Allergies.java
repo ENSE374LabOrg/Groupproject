c7f1c16b0019a687cb02bd6c6e84dbed3b7e7d96
burns26c <burns26c@10.0.2.15>
2016-11-01 17:04:47 -0600

    made the classes for the allergies and nutrient

---
+++ src/Allergies.java
@@ -0,0 +1,22 @@
+
+public class Allergies {
+private String Allergies;
+	
+	public Allergies() {
+		this.Allergies= "";
+		
+	}
+	
+	public Allergies(String Allergies){
+		
+		this.Allergies= Allergies;
+	}
+public String get(){
+	return Allergies;
+}
+public void setNutrients(String Allergies){
+	this.Allergies=Allergies;
+}
+
+}
+
---
+++ src/Nutrients.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author burns26c
+ *
+ */
+public class Nutrients {
+
+	private String Nutrients;
+	
+	public Nutrients() {
+		this.Nutrients= "";
+		
+	}
+	
+	public Nutrients(String Nutrients){
+		
+		this.Nutrients= Nutrients;
+	}
+public String getNutrients(){
+	return Nutrients;
+}
+public void setNutrients(String Nutrients){
+	this.Nutrients=Nutrients;
+}
+
+}
+
+
