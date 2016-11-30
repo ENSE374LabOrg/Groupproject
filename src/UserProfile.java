2dd17a061f6164013716efe673cf664a49f5ce50
Shamsco <shamissa@10.0.2.15>
2016-11-30 17:55:08 -0600

    Fixed a bunch of problems

---
+++ src/UserProfile.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserProfile {
+	
+	private String Username;
+	private String Password;
+	private List<String> Preferences;
+	private  List<String> Allergies;
+
+	public UserProfile(){
+		this.Username="default";
+		this.setPassword("default");
+		this.Preferences=new ArrayList<String>(); 
+	}
+	public UserProfile(String Username, String Password,List<String> Preferences) {
+		this.Username=Username;
+		this.Password=Password;
+		this.Preferences= Preferences;
+	}
+	public String getUsername(){
+		return Username;
+			 
+	}
+	public void setUsername(String Username, String Password){
+		this.Username=Username;
+		this.setPassword(Password);
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public void setPreferences(List<String> Preferences){
+		this.Preferences=Preferences;
+	}
+	public List<String> getPreferences(){
+		return Preferences;
+	}
+
+	public void setAllergies(List<String> Allergies){
+		this.Allergies=Allergies;
+	}
+	public List<String> getAllergies(){
+		return Allergies;
+	}
+
+	public boolean Match(String Password){	
+		if (this.Password==Password){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+ 
+
--- src/program.java
+++ src/program.java
@@ -22,18 +22,19 @@ public class program {
 		try {
 
 			//You can change paths or filenames to make the Exception hit.
-			Scanner in = new Scanner(Paths.get("randomdata.csv"));
+			Scanner file = new Scanner(Paths.get("randomdata.csv"));
+			Scanner in = new Scanner(System.in);
 			int input = 0;
 			String username;
 			String password;
 			String userinput;
 			ArrayList<Food> match = new ArrayList<Food>();
 			boolean loggedin = false;
-			while (in.hasNextLine()) {
+			while (file.hasNextLine()) {
 				Food item = new Food();
 				ArrayList<Nutrients> nutrients = new ArrayList<Nutrients>();
 				ArrayList<String> allergies = new ArrayList<String>();
-				String externalLine = in.nextLine();
+				String externalLine = file.nextLine();
 				String [] parts = externalLine.split("\\|");
 				String name = parts[0];
 				String nutrient = parts[1];
@@ -44,7 +45,7 @@ public class program {
 				info.addItem(item);
 			}
 			do{
-				if(loggedin = false){
+				if(loggedin == false){
 					System.out.println("What Would you like to do?");
 					System.out.println("1.Login:");
 					System.out.println("2.Filter by nutrient:");
@@ -52,7 +53,7 @@ public class program {
 					System.out.println("9.END");
 					input = in.nextInt();
 				}
-				if(loggedin = true){
+				if(loggedin == true){
 					System.out.println("What Would you like to do?");
 					System.out.println("2.Filter by nutrient:");
 					System.out.println("3.Filter by allergy:");
@@ -61,24 +62,27 @@ public class program {
 				}
 				switch(input){
 				case 1:
+					in.nextLine();
 					System.out.println("Enter Username:");
-					username = in.next();
+					username = in.nextLine();
+					in.nextLine();
 					System.out.println("Enter Password:");
-					password = in.next();
-					
+					password = in.nextLine();
 					loggedin = true;
 					break;
 				case 2:
+					in.nextLine();
 					System.out.println("Enter nutrient to filter by:");
-					userinput = in.next();
+					userinput = in.nextLine();
 					match = info.matchNutrient(userinput);
 					for(int i = 0; i < match.size() ; i++){
 						System.out.println("Matches found for nutrients: " + match.get(i).getName());
 					}
 					break;
 				case 3:
+					in.nextLine();
 					System.out.println("Enter allergy to filter by:");
-					userinput = in.next();
+					userinput = in.nextLine();
 					match = info.matchNutrient(userinput);
 					for(int i = 0; i < match.size() ; i++){
 						System.out.println("Matches found for allergies:" + match.get(i).getName());
@@ -90,24 +94,13 @@ public class program {
 					break;
 				}
 				
-				
 			}while(input != 9);
-			
-		
-			
-			info.removeItem("Apple");
 			info.printlistfullinfo();
 		}
-		 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
-		
-		
-		
 	}
 
 
